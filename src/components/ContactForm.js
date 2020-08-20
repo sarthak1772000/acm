392e73a57541617c881523de56a3886bb4a9026d
Nisha Jadhav <57458313+Nisha1607@users.noreply.github.com>
2020-08-20 13:38:01 +0530

    ContactUsPage3
    Contact Us form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,81 +1,125 @@
-import React, { useState, useEffect } from "react";
-import './fontawesome'
-import "./signup.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Signup = () => {
+import React, { Component } from 'react';
+import './fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Container } from 'reactstrap';
+
+class Form extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state={
+            username:'',
+            email:'',
+            subject:'',
+            message:''
+        }
+    }
+
+    handleUsernameChange=(event)=>{
+        this.setState({
+            username:event.target.value
+        })
+    }
+    
+    handleEmailChange=(event)=>{
+        this.setState({
+           email:event.target.value
+        })
+    }
+    handleSubjectChange=(event)=>{
+        this.setState({
+            subject:event.target.value
+        })
+    }
+    handleMessagetChange=(event)=>{
+        this.setState({
+            message:event.target.value
+        })
+    }
+    handleSubmit=(event)=>{
+       
+       alert(`${this.state.username} ${this.state.email} ${this.state.subject}`)
+       event.preventDefault();
+    }
+
+    render() {
     return (
-        <React.Fragment>
-            <div title="Signup Form" />
-            <div className="row justify-content-center">
-                <div className="col-lg-6 text-center">
-                   <div className="card bg-primary">
+    <Container>
+        <div className="card bg-light">
                         <article
                             className="card-body mx-auto"
                             style={{ maxWidth: "400px" }}
                         >
-                            <h4 className="card-title mt-3 text-center">
+                            <h4 className="card-title mt-3 text-center form-header" style={{borderBottom:'2px solid #49a1eb'}}>
                               Contact Us
                             </h4>
                             <p className="text-center">
                                Feel free to get in touch!
                             </p>
-                            <form>
-                                <div className="form-group input-group">
-                                    <div className="input-group-prepend">
-                                        <span className="input-group-text">
-                                            <FontAwesomeIcon icon="user" />
-                                        </span>
-                                    </div>
-                                    <input
-                                        name=""
-                                        className="form-control"
-                                        placeholder="Your name"
-                                        type="text"
-                                    />
-                                </div>
-                                <div className="form-group input-group">
-                                    <div className="input-group-prepend">
-                                        <span className="input-group-text">
-                                            <FontAwesomeIcon icon="envelope" />
-                                        </span>
-                                    </div>
-                                    <input
-                                        name=""
-                                        className="form-control"
-                                        placeholder="Your Email address"
-                                        type="email"
-                                    />
-                                </div>
-                                <div className="form-group input-group">
-                                    <div className="input-group-prepend">
-                                        <span className="input-group-text">
-                                            <FontAwesomeIcon icon="tags" />
-                                        </span>
-                                    </div>
-                                    <input
-                                        className="form-control"
-                                        placeholder="Subject"   
-                                    />
-                                    <span className="password-toogle-icon">
-                                       {}
-                                    </span>
-                                </div>
-
-                                <div className="form-group input-group">
-                                    <div className="input-group-prepend">
-                                        <span className="input-group-text">
-                                            <FontAwesomeIcon icon="edit" />
-                                        </span>
-                                    </div>
-                                    <textarea
-                                        name=""
-                                        className="form-control"
-                                        placeholder="Your message"
-                                        type="textarea"
-                                    />
-                                </div>
-                                <div className="form-group">
+     <form onSubmit={this.handleSubmit}>
+         
+        <div className="form-group input-group">
+             <div className="input-group-prepend">
+                <span className="input-group-text">
+                        <FontAwesomeIcon icon="user" />
+               </span>
+            </div>
+            <input
+                 name=""
+                 className="form-control"
+                 placeholder="Your name"
+                 type="text"
+                 value={this.state.username}
+                 onChange={this.handleUsernameChange}
+            />
+        </div>
+        <div className="form-group input-group">
+             <div className="input-group-prepend">
+                <span className="input-group-text">
+                        <FontAwesomeIcon icon="envelope" />
+               </span>
+            </div>
+            <input
+                 name=""
+                 className="form-control"
+                 placeholder="Your email"
+                 type="text"
+                 value={this.state.email}
+                 onChange={this.handleEmailChange}
+            />
+        </div>
+        <div className="form-group input-group">
+             <div className="input-group-prepend">
+                <span className="input-group-text">
+                        <FontAwesomeIcon icon="tags" />
+               </span>
+            </div>
+            <input
+                 name=""
+                 className="form-control"
+                 placeholder="subject"
+                 type="text"
+                 value={this.state.subject}
+                 onChange={this.handleSubjectChange}
+            />
+        </div>
+        <div className="form-group input-group">
+             <div className="input-group-prepend">
+                <span className="input-group-text">
+                        <FontAwesomeIcon icon="tags" />
+               </span>
+            </div>
+            <textarea
+                 name=""
+                 className="form-control"
+                 placeholder="message"
+                 type="textarea"
+                 value={this.state.message}
+                 onChange={this.handleMessagetChange}
+            />
+        </div>
+        <div className="form-group">
                                     <button
                                         type="submit"
                                         className="btn btn-primary btn-block"
@@ -84,16 +128,12 @@ const Signup = () => {
                                         Send{" "}
                                     </button>
                                 </div>
-                                
-                            </form>
-                        </article>
+     </form>
+     </article>
                     </div>
 
-                   
-                </div>
-            </div>
-        </React.Fragment>
+     </Container>
     );
-};
-
-export default Signup;
+  }
+}
+export default Form;
