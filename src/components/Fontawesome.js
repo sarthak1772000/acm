eb98317ae88da675e488f53332f9748e75fe9cb7
Nisha Jadhav <57458313+Nisha1607@users.noreply.github.com>
2020-08-20 13:35:23 +0530

    FontAwesome page for contactUsPage
    fontAwesomePage

---
+++ src/components/Fontawesome.js
@@ -0,0 +1,55 @@
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {
+  faUser,
+  faEnvelope,
+  faLock,
+  faEye,
+  faEyeSlash,
+  faArrowUp,
+  faArrowDown,
+  faExpand,
+  faPen,
+  faCompress,
+  faBolt,
+  faSpinner,
+  faTags,
+  faEdit
+} from '@fortawesome/free-solid-svg-icons'
+
+
+
+import {
+  faFacebookSquare,
+  faTwitter,
+  faLinkedin,
+  
+  faSkype,
+  faGoogle,
+  faInstagramSquare,
+  faYoutube,
+  
+} from '@fortawesome/free-brands-svg-icons'
+
+library.add(
+  faUser,
+  faEnvelope,
+  faLock,
+  faEye,
+  faPen,
+  faEdit,
+  faEyeSlash,
+  faArrowUp,
+  faArrowDown,
+  faExpand,
+  faCompress,
+  faBolt,
+  faSpinner,
+  faFacebookSquare,
+  faTwitter,
+  faLinkedin,
+  faSkype,
+  faGoogle,
+  faInstagramSquare,
+  faYoutube,
+  faTags
+)
